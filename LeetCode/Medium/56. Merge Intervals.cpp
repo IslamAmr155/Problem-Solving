8904c787e47d50ae33552c44ef92c9829a7aad1c
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2023-10-18 18:25:12 +0300

    18/10

---
+++ LeetCode/Medium/56. Merge Intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        int n = intervals.size(), j = 0;
+        for (int i=1;i<n;i++)
+        {
+            if (intervals[i][0] <= ans[j][1])
+                ans[j][1] = max(intervals[i][1], ans[j][1]);
+            else
+            {
+                ans.push_back(intervals[i]);
+                j++;
+            }
+        }
+        return ans;
+    }
+};

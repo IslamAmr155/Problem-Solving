9e64b1780e0b167f859f4565708f18edcee47a18
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2024-01-18 19:55:25 +0200

    18/1

--- LeetCode/Easy/1207. Unique Number of Occurrences.cpp
+++ LeetCode/Easy/1207. Unique Number of Occurrences.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int> m;
-        sort(arr.begin(),arr.end());
-        for (int i=0;i<arr.size();i++)
+        for (int x : arr)
+            m[x]++;
+        set<int> s;
+        for (auto i = m.begin(); i != m.end(); i++)
         {
-            int c = 1;
-            while (i < arr.size()-1 && arr[i] == arr[i+1])
-            {
-                c++;
-                i++;
-            }
-            if(!m[c])
-                m[c]++;
-            else
+            if (s.find(i->second) != s.end())
                 return false;
+            s.insert(i->second);
         }
         return true;
     }

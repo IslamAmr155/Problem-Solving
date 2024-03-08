f39005349960d858533ecc79dce24dc315be1086
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2024-03-08 02:08:33 +0200

    8/3

---
+++ LeetCode/Hard/2709. Greatest Common Divisor Traversal.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int getf(vector<int>& f, int x)
+    {
+        return f[x] == x ? x : (f[x] = getf(f,f[x]));
+    }
+
+    void merge(vector<int>& f, vector<int>& num, int x, int y)
+    {
+        x = getf(f,x);
+        y = getf(f,y);
+        if (x == y) return;
+        if (num[x] < num[y]) swap(x,y);
+        f[y] = x;
+        num[x] += num[y];
+    }
+
+    bool canTraverseAllPairs(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return true;
+        vector<int> f(n), num(n);
+        for (int i = 0;i<n;i++)
+        {
+            f[i] = i;
+            num[i] = 1;
+        }
+        unordered_map<int,int> m;
+        for (int i = 0;i<n;i++)
+        {
+            int x = nums[i];
+            if (x == 1) return false;
+            for (int d = 2; d * d <= x; d++)
+            {
+                if (x % d == 0)
+                {
+                    if (m.count(d)) merge (f,num,i,m[d]);
+                    else m[d] = i;
+                    while (x % d == 0) x /= d;
+                }
+            }
+            if (x > 1)
+            {
+                if (m.count(x)) merge(f,num,i,m[x]);
+                else m[x] = i;
+            }
+        }
+        return num[getf(f,0)] == n;
+    }
+};

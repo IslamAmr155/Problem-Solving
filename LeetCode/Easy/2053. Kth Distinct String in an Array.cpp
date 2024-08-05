3d95973740d7620e5e97562f5871d1ac9909ce64
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2024-08-05 22:09:21 +0300

    5/8

---
+++ LeetCode/Easy/2053. Kth Distinct String in an Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string,int> m;
+        for (string &s : arr) m[s]++;
+        for (string &s : arr)
+            if (m[s] == 1 && --k == 0) return s;
+        return "";
+    }
+};

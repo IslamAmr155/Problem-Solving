bb2861ce1f08bcbc179629796fc080c7f1b13ec4
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2024-12-01 19:57:18 +0200

    1/12

---
+++ LeetCode/Easy/1346. Check If N and Its Double Exist.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        set<int> s;
+        for (int& x : arr) {
+            if (s.contains(x * 2) || (x % 2 == 0 && s.contains(x / 2))) return true;
+            s.insert(x); 
+        }
+        return false;
+    }
+};

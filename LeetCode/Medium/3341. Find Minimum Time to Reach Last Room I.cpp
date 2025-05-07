a676d5acd8f2848bcd3100264d951b6394ba811a
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2025-05-07 05:32:15 +0300

    7/5

---
+++ LeetCode/Medium/3341. Find Minimum Time to Reach Last Room I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> q;
+        q.push({0, 0, 0});
+        moveTime[0][0] = 2e9;
+        int n = moveTime.size(), m = moveTime[0].size();
+        vector<int> v = q.top(), d = {1, 0, -1, 0, 1};
+        while (q.top()[1] != n-1 || q.top()[2] != m-1) {
+            v = q.top();
+            q.pop();
+            for (int i = 0;i<4;i++) {
+                int x = v[1] + d[i], y = v[2] + d[i+1];
+                if (min(x,y) != -1 && x != n && y != m) {
+                    q.push({max(moveTime[x][y]+1, v[0]+1), x, y});
+                    moveTime[x][y] = 2e9;
+                }
+            }
+        }
+        return q.top()[0];
+    }
+};
